6b340d7151b1e074a0143aa8ea846b8a74d67c85
Akarsh <akarsh@gorismo.com>
2021-07-02 18:38:24 +0530

    login screen initial commit

--- chatAppDemo_Rahul/Controllers/LoginViewController.swift
+++ chatAppDemo_Rahul/Controllers/LoginViewController.swift
@@ -8,22 +8,51 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "login")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    private let textField: UITextField = {
+        let textField = UITextField()
+        textField.text = "Some Text here"
+        return textField
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        title = "Log In"
+        view.backgroundColor = .white
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Register",
+                                                            style: .done,
+                                                            target: self,
+                                                            action: #selector(didTapRegister))
+        
+        view.addSubview(imageView)
+        view.addSubview(textField)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let size = view.frame.size.width/3
+        imageView.frame = CGRect(x: (view.frame.size.width-size)/2, y: 100, width: size, height: size)
+        
+        textField.frame = CGRect(x: (view.frame.size.width-size)/2, y: size + 100, width: size, height: size)
+        
     }
-    */
+    
+    
+    
+    @objc private func didTapRegister() {
+        let vc = RegisterViewController()
+        vc.title = "Create Account"
+        navigationController?.pushViewController(vc, animated: true)
+    }
 
 }
--- chatAppDemo_Rahul/Controllers/RegisterViewController.swift
+++ chatAppDemo_Rahul/Controllers/RegisterViewController.swift
@@ -5,4 +5,11 @@
 //  Created by Akarsh Agrawal on 29/06/21.
 //
 
-import Foundation
+import UIKit
+
+class RegisterViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
